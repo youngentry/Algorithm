0afba32e5708d516e426fac7ce8b16bb886ef684
young <gentry_@naver.com>
2024-01-05 22:55:21 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+for T_C in range(T):
+    brackets = list(input())
+    stack = []
+    for bracket in brackets:
+        if len(stack) and stack[-1] == "(" and bracket ==")":
+            stack.pop()
+        else:
+            stack.append(bracket)
+    
+    if not len(stack):
+        print("YES")
+    else:
+        print("NO")
