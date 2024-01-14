1dc24a36c26758aae857d72973173d194ecf0b0e
young <gentry_@naver.com>
2024-01-14 14:41:57 +0900

    [Gold V] Title: 토마토, Time: 944 ms, Memory: 145928 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -12,45 +12,35 @@ wall = ["0"]*(m+2)
 box.append(wall)
 for _ in range(n):
   row = list(map(int, input().split()))
-  box.append(["0"] +row + ["0"])
+  box.append(["0"] + row + ["0"])
 box.append(wall)
-# print(box)
 
+# BFS
 def bfs(starts):
   queue = deque(starts)
   max_count = 0
-  # print(queue)
   while len(queue):
     x,y,count = queue.popleft()
     max_count = count
-    # print(x,y,count,"?")
     for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
       nx,ny = x+dx, y+dy
-      # print(nx,ny,"123", box[nx][ny])
       if box[nx][ny] == 0:
         box[nx][ny] = 1
         queue.append((nx,ny,count+1))
   else:
     return max_count
 
+# 시작지점 탐색
 def findStarts():
   starts = []
   for i in range(len(box)):
     for j in range(len(box[0])):
       if box[i][j]==1:
         starts.append((i,j,0))
-
   return starts
 
-starts = findStarts()
-
-# print(starts)
-
-result = bfs(starts)
-
-# print(box)
-
-def checkBox():
+# 익지 않은 도마도 확인
+def checkUnripenTomatos():
   isRipen = True
   for i in range(len(box)):
     for j in range(len(box[0])):
@@ -61,7 +51,6 @@ def checkBox():
   if not isRipen:
     return -1
 
-if checkBox() == -1:
-  print(-1)
-else:
-  print(result)
+starts = findStarts() # 시작 지점 찾기
+result = bfs(starts) # queue에 시작지점 넣고 bfs
+print(-1) if checkUnripenTomatos() == -1 else print(result) # 결과 출력
