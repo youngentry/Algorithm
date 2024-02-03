9faf3a0334fd6aacd2fc149a52b2d346fa178fc7
young <gentry_@naver.com>
2024-02-03 18:24:54 +0900

    [Silver IV] Title: 수들의 합 2, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,5 +1,4 @@
 import sys
-
 input = sys.stdin.readline
 
 n,m = map(int,input().split())
@@ -10,13 +9,16 @@ left, right = 0,0
 cur_sum = 0
 count =0
 while left<n:
-    # print("cur:",cur_sum,count,"L:",left,"R:",right,)
+    # left:0, right:0 인덱스에서 시작
+    # cur_sum이 m보다 작다면, 현재 인덱스 숫자를 더하고, right 다음으로
     if right!=n and cur_sum < m :
         cur_sum += numbers[right]
         right+=1
     else:
+    # 그 외에는 cur_sum이 m보다 커진 경우이니 현재 인덱스 숫자를 빼고 left 다음으로
         cur_sum -= numbers[left]
         left+=1
+
     if cur_sum == m:
         count+=1
 
