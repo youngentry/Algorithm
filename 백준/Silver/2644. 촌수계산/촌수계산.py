bb12e5c619a90e11d92084ce11ad09dbad0d2ffc
young <gentry_@naver.com>
2024-02-09 12:47:56 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -10,31 +10,27 @@ for _ in range(m):
     high, low = map(int,input().split())
     relation[high].append(low)
 
-# print(relation)
-
 stack = []
 result = 987654321
 for i in range(1,n+1):
-    # print("---------------")
     stack.append([i,1])
     a_count = b_count = None
+    # print(relation)
     while stack:
         high, move = stack.pop()
-        # print("high:",high,move)
-        # print(high,relation)
-        if high ==a:
+        # print(high,low,move,'b')
+        if high == a:
             a_count = move-1
-        elif high ==b:
+        elif high == b:
             b_count = move-1
         for low in relation[high]:
-            # print(low,a,b, low==a, low==b)
-            if low == a:
-                a_count = move+1
-            elif low == b:
-                b_count = move+1
+            # if low == a:
+            #     a_count = move+1
+            # elif low == b:
+            #     b_count = move+1
             stack.append([low,move+1])
+        # print(high,low,move,'a')
 
-        # print(a_count,b_count,"???????????")
     if a_count != None and b_count != None:
         if result>a_count+b_count:
             result = a_count+b_count
