81fefb0ad02245efe7a8ac5341d14f5162480ee6
young <gentry_@naver.com>
2024-03-29 11:50:49 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 112056 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,42 +1,51 @@
 import sys
+input = sys.stdin.readline
 from heapq import heappush, heappop
 
 INF = int(2e9)
-def dijkstra(sx):
-    global mx_time
 
-    v = [INF]*(N+1)
+def dijkstra(graph, sx, N):
+    v = [INF] * (N + 1)
     v[sx] = 0
-    pq = [(0,sx)]
+    pq = [(0, sx)]
 
     while pq:
-        w,x = heappop(pq)
+        w, x = heappop(pq)
 
         if w > v[x]:
             continue
 
-        for dw,nx in graph[x]:
-            nw =w+dw
+        for dw, nx in graph[x]:
+            nw = w + dw
             if nw < v[nx]:
                 v[nx] = nw
-                heappush(pq,(nw,nx))
+                heappush(pq, (nw, nx))
     return v
 
+# 입력 받기
+input = sys.stdin.readline
+N, M, X = map(int, input().split())
 
-N,M,X = map(int,input().split())
-
-students = [ i for i in range(1,N+1)]
+# 그래프와 역방향 그래프 초기화
 graph = [[] for _ in range(N+1)]
-for i in range(M):
-    s,e,w = map(int,input().split())
-    graph[s].append((w,e))
+reverse_graph = [[] for _ in range(N+1)]
 
+# 그래프 입력 받기 및 역방향 그래프 생성
+for _ in range(M):
+    s, e, w = map(int, input().split())
+    graph[s].append((w, e))
+    reverse_graph[e].append((w, s))  # 역방향 그래프에 추가
+
+# X에서 시작하는 다익스트라 알고리즘 실행
+dist_from_X = dijkstra(graph, X, N)
+
+# 역방향 그래프에서 X로의 다익스트라 알고리즘 실행
+dist_to_X = dijkstra(reverse_graph, X, N)
+
+# 최대 시간 계산
 mx_time = 0
-base_v = dijkstra(X)
-for num in students:
-    if num != X:
-        temp_v = dijkstra(num)
-        if temp_v[X] != INF:
-            mx_time = max(mx_time, temp_v[X]+base_v[num])
+for i in range(1, N+1):
+    if i != X:
+        mx_time = max(mx_time, dist_from_X[i] + dist_to_X[i])
 
 print(mx_time)
