878e52e5a19b2c615808f854479c378b15456cf8
young <gentry_@naver.com>
2024-09-06 14:27:39 +0900

    [Silver III] Title: N과 M (1), Time: 140 ms, Memory: 112492 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+path = []
+v = [0]*(N+1)
+def dfs(n):
+    if n == M:
+        print(" ".join(list(map(str,path))))
+        return
+
+    for i in range(1, N+1):
+        if not v[i]:
+            v[i] = 1
+            path.append(i)
+            dfs(n+1)
+            v[i] = 0
+            path.pop()
+
+dfs(0)
