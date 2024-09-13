8611a6cce0db80576dc82f658431fb52b667e8fc
young <gentry_@naver.com>
2024-09-13 13:01:53 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 156 ms, Memory: 131396 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
@@ -1,32 +1,9 @@
-import heapq
 import sys
-from _collections import deque
-from copy import deepcopy
-from heapq import heappush
+input = sys.stdin.readline
 
 N,M = map(int,input().split())
 heights = [0] + list(map(int,input().split())) + [0]
 orders = []
-# for _ in range(M):
-#     orders.append(list(map(int,input().split())))
-# orders.sort()
-# print(orders)
-#
-# acc = 0
-# orders = deque(orders)
-# hq = []
-#
-# for i in range(1,100_001):
-#     while orders and orders[0] == i:
-#         sidx,fidx,val = orders.popleft()
-#         acc += order[sval]
-#         heappush(hq,(fidx,val))
-#     # while 시작이 같으면 acc ++ 하고 heap에 (종료,acc) 추가
-#     # while 종료가 같으면 acc -- 하고 heap에서 제거
-#
-#     heights[i] += acc # 최종 높이를 적용
-#     pass
-
 arr = [0]*(N+1) + [0]
 
 for i in range(M):
@@ -40,6 +17,6 @@ acc = 0
 while idx <= N:
     idx += 1
     acc += arr[idx]
-
     heights[idx] += acc
+    
 print(' '.join(map(str,heights[1:-1])))
