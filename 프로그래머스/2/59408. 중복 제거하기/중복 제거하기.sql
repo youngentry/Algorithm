d6bacade9c297ae783188e3f5d71b82c5f7bc439
young <gentry_@naver.com>
2024-10-11 03:32:34 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT COUNT(DISTINCT NAME)
+FROM ANIMAL_INS 
+
+# ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
