d6746a79c1c58c899cc25c3219dcbec41efc9ca7
young <gentry_@naver.com>
2024-10-11 03:43:35 +0900

    [level 1] Title: 이름이 있는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME != ''
+
+# SELECT 열_이름
+# 	FROM 테이블_이름
+# 	WHERE 조건식
+# 	GROUP BY 열_이름
+# 	HAVING 조건식
+# 	ORDER BY 열_이름
+# 	LIMIT 숫자
+
+
+# ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
