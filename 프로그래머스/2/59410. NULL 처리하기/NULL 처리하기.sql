fbe8422f66a5689c9f717256fe5e1d86e4cbc1cd
young <gentry_@naver.com>
2024-10-20 13:41:33 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT 
+    ANIMAL_TYPE, 
+    COALESCE(NAME, 'No name') AS NAME,
+    SEX_UPON_INTAKE
+FROM ANIMAL_INS 
+ORDER BY ANIMAL_ID
+
+
+
+# ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
