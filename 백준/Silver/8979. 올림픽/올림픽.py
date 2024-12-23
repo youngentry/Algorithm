46f40997265668cdb7c894cb86d49f83bd2890ab
young <gentry_@naver.com>
2024-12-23 19:45:15 +0900

    [Silver V] Title: 올림픽, Time: 88 ms, Memory: 108384 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,49 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int,input().split())
+
+nations = []
+
+for i in range(N):
+    nations.append(list(map(int,input().split())))
+
+nations.sort(key=lambda x:(x[1],x[2],x[3]), reverse=True)
+
+rank = 1
+skip_count = 1
+for i in range(N-1):
+    # 금메달 수가 더 많은 경우
+    if nations[i][1] > nations[i+1][1]:
+        if nations[i][0] == K:
+            print(rank)
+            break
+        else:
+            rank += skip_count
+            skip_count = 1
+            continue
+
+    # 금메달 수는 같은데
+    # 은메달 수가 더 많은 경우
+    if nations[i][2] > nations[i + 1][2]:
+        if nations[i][0] == K:
+            print(rank)
+            break
+        else:
+            rank += skip_count
+            skip_count += 1
+            continue
+
+    # 금, 은메달 수가 같고
+    # 동메달 수가 더 많은 경우
+    if nations[i][3] > nations[i + 1][3]:
+        if nations[i][0] == K:
+            print(rank)
+            break
+        else:
+            rank += skip_count
+            skip_count += 1
+
+    skip_count += 1
+
